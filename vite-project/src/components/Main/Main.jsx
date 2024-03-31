f45b361fefd47c5a176d20115ab906f9d82758bc
ReginaZaets <regina.alibaeva21@gmail.com>
2024-03-31 10:05:08 +0300

    hw react 1

--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -1,23 +1,21 @@
-import Column from "../Column/Column"
+import Column from "../Column/Column";
 
-function Main () {
-    return (
-        <main className="main">
-			<div className="container">
-				
-				<div className="main__block">
-					<div className="main__content">
-						<Column name={"Без статуса"}/>
-						<Column name={"Нужно сделать"}/>
-						<Column name={"В работе"}/>
-						<Column name={"Тестирование"}/>
-						<Column name={"Готово"}/>
-					</div>
-				
-				</div>
-			</div>
-		</main>
-    )
+function Main() {
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="main__block">
+          <div className="main__content">
+            <Column name={"Без статуса"} />
+            <Column name={"Нужно сделать"} />
+            <Column name={"В работе"} />
+            <Column name={"Тестирование"} />
+            <Column name={"Готово"} />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
